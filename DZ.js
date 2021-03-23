a98f1ea5e05665a0ec12dee4bafdd9b9c69b926f
Alexey Boklogov <kashira87@mail.ru>
2021-03-23 17:25:34 +0300

    11/2
    Создал файл для решения ДЗ-2 (модуль 2)

---
+++ DZ.js
@@ -0,0 +1,16 @@
+// // 11/2
+// Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово.
+// Объявлена функция calculateEngravingPrice(message, pricePerWord). Эта функция принимает строку, состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord).
+// Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Пиши код ниже этой строки
+// const array = message.split(' ');
+// const arrayLength = array.length;
+// return arrayLength * pricePerWord;
+//   // Пиши код выше этой строки
+// }
+// console.log(calculateEngravingPrice('JavaScript у меня в крови', 10));
+
+// 12/2
+// Задание
+// Дополни код функции makeStringFromArray(array, delimeter) так, чтобы она возвращала в переменной string результат соединения элементов массива array c разделителем delimeter - строку.

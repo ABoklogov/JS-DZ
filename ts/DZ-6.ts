df55c7feead548d5fb7b011fde3fbb792150f231
Алексей Боклогов <kashira87@mail.ru>
2023-06-16 16:58:27 +0300

    Типизация половины задач в DZ-6

--- ts/DZ-6.ts
+++ ts/DZ-6.ts
@@ -1,18 +1,17 @@
 // 2 - 6
-// function filterArray(numbers, value) {
-//     const filteredNumbers = [];
-//     // Пиши код ниже этой строки
+// type FilterArray = (numbers: number[], value: number) => ;
 
-//     numbers.forEach(num => {
-//         if (num > value) {
-//             filteredNumbers.push(num)
-//         } 
-//     })
-
-//     // Пиши код выше этой строки
-//     return filteredNumbers;
-// }
-  
+// const filterArray: FilterArray = function (numbers, value) {
+//   const filteredNumbers: number[] = [];
+//   // Пиши код ниже этой строки
+//   numbers.forEach(num => {
+//     if (num > value) {
+//       filteredNumbers.push(num);
+//     };
+//   });
+//   // Пиши код выше этой строки
+//   return filteredNumbers;
+// };
 // console.log(filterArray([1, 2, 3, 4, 5], 3)); // возвращает [4, 5].
 
 // 3 - 6
@@ -20,39 +19,42 @@
 // Функция getCommonElements(firstArray, secondArray) принимает два массива произвольной длины в параметры firstArray и secondArray, и возвращает новый массив их общих элементов, то есть тех которые есть в обоих массивах.
 
 // Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+// type GetCommonElements = (firstArray: any[], secondArray: any[]) => any[];
 
-// function getCommonElements(firstArray, secondArray) {
-//     const commonElements = [];
-//     // Пиши код ниже этой строки
-  
-//     firstArray.forEach(num => {
-//         if (secondArray.includes(num)) {
-//         commonElements.push(num);
-//       }
-//     })
-  
-//     return commonElements;
-//     // Пиши код выше этой строки
-//   }
+// const getCommonElements: GetCommonElements = function (firstArray, secondArray) {
+//   const commonElements: any[] = [];
+//   // Пиши код ниже этой строки
+
+//   firstArray.forEach(num => {
+//     if (secondArray.includes(num)) {
+//       commonElements.push(num);
+//     }
+//   })
+
+//   return commonElements;
+//   // Пиши код выше этой строки
+// };
 
 // 9-6
 // Задание
 // Функция changeEven(numbers, value) принимает массив чисел numbers и обновляет каждый элемент, значение которого это чётное число, добавляя к нему значение параметра value.
 
 // Выполни рефакторинг функции так, чтобы она стала чистой - не изменяла массив чисел numbers, а создавала, наполняла и возвращала новый массив с обновлёнными значениями.
-// function changeEven(numbers, value) {
-//     // Пиши код ниже этой строки
-//     const newNumbers = [];
-  
-//     numbers.forEach(element => {
-//         if (element % 2 === 0) {
-//             element += value;
-//         }
-//         newNumbers.push(element);
-//     })
+// type ChangeEven = (numbers: number[], value: number) => number[];
+
+// const changeEven: ChangeEven = function (numbers, value) {
+//   // Пиши код ниже этой строки
+//   const newNumbers: number[] = [];
+
+//   numbers.forEach(element => {
+//     if (element % 2 === 0) {
+//       element += value;
+//     }
+//     newNumbers.push(element);
+//   })
 //   return newNumbers;
-//     // Пиши код выше этой строки
-// }
+//   // Пиши код выше этой строки
+// };
 
 // 15 - 6
 // const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
@@ -69,8 +71,13 @@
 // Используя метод filter() дополни код так, чтобы:
 // В переменной topRatedBooks получился массив книг рейтинг которых (свойство rating) больше либо равно значению переменной MIN_RATING.
 // В переменной booksByAuthor получился массив книг написанных автором с именем(свойство author) которое совпадает со значением в переменной AUTHOR.
+// interface Book {
+//   readonly title: string;
+//   readonly author: string;
+//   readonly rating: number;
+// };
 
-// const books = [
+// const books: Book[] = [
 //   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
 //   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
 //   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
@@ -93,7 +100,18 @@
 // Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она возвращала массив пользователей у которых есть друг с именем в параметре friendName. Массив друзей пользователя хранится в свойстве friends.
 
 // Пиши код ниже этой строки
-// const users = [
+// interface User {
+//   readonly name: string;
+//   readonly email: string;
+//   readonly eyeColor: ('blue' | 'green' | 'brown');
+//   readonly friends: string[];
+//   readonly isActive: boolean;
+//   readonly balance: number;
+//   readonly gender: ('male' | 'female');
+//   readonly age: number;
+// };
+
+// const users: User[] = [
 //   {
 //     name: 'Moore Hensley',
 //     email: 'moorehensley@indexia.com',
@@ -164,14 +182,17 @@
 //     gender: 'female',
 //     age: 39
 //   }
-// ]
+// ];
+
 // const friendName = 'Sharron Pace';
 
-// const getUsersWithFriend = (users, friendName) => {
+// type GetUsersWithFriend = (users: User[], friendName: string) => User[];
+
+// const getUsersWithFriend: GetUsersWithFriend = (users, friendName) => {
 //   return users.filter(user => {
-//    const arrFrends = user.friends;
-//    return arrFrends.includes(friendName) 
-//  })
+//     const arrFrends = user.friends;
+//     return arrFrends.includes(friendName)
+//   })
 // };
 // // Пиши код выше этой строки
 
@@ -180,92 +201,109 @@
 // 21 - 6
 // Задание
 // Дополни функцию getFriends(users) так, чтобы она возвращала массив друзей всех пользователей (свойство friends). У нескольких пользователей могут быть одинаковые друзья, сделай так чтобы возвращаемый массив не содержал повторений.
+interface User {
+  readonly name: string;
+  readonly email: string;
+  readonly eyeColor: ('blue' | 'green' | 'brown');
+  readonly friends: string[];
+  readonly isActive: boolean;
+  readonly balance: number;
+  readonly gender: ('male' | 'female');
+  readonly age: number;
+};
 
-// const users = [
-//   {
-//     name: 'Moore Hensley',
-//     email: 'moorehensley@indexia.com',
-//     eyeColor: 'blue',
-//     friends: ['Sharron Pace'],
-//     isActive: false,
-//     balance: 2811,
-//     gender: 'male',
-//     age: 37
-//   },
-//   {
-//     name: 'Sharlene Bush',
-//     email: 'sharlenebush@tubesys.com',
-//     eyeColor: 'blue',
-//     friends: ['Briana Decker', 'Sharron Pace'],
-//     isActive: true,
-//     balance: 3821,
-//     gender: 'female',
-//     age: 34
-//   },
-//   {
-//     name: 'Ross Vazquez',
-//     email: 'rossvazquez@xinware.com',
-//     eyeColor: 'green',
-//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//     isActive: false,
-//     balance: 3793,
-//     gender: 'male',
-//     age: 24
-//   },
-//   {
-//     name: 'Elma Head',
-//     email: 'elmahead@omatom.com',
-//     eyeColor: 'green',
-//     friends: ['Goldie Gentry', 'Aisha Tran'],
-//     isActive: true,
-//     balance: 2278,
-//     gender: 'female',
-//     age: 21
-//   },
-//   {
-//     name: 'Carey Barr',
-//     email: 'careybarr@nurali.com',
-//     eyeColor: 'blue',
-//     friends: ['Jordan Sampson', 'Eddie Strong'],
-//     isActive: true,
-//     balance: 3951,
-//     gender: 'male',
-//     age: 27
-//   },
-//   {
-//     name: 'Blackburn Dotson',
-//     email: 'blackburndotson@furnigeer.com',
-//     eyeColor: 'brown',
-//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//     isActive: false,
-//     balance: 1498,
-//     gender: 'male',
-//     age: 38
-//   },
-//   {
-//     name: 'Sheree Anthony',
-//     email: 'shereeanthony@kog.com',
-//     eyeColor: 'brown',
-//     friends: ['Goldie Gentry', 'Briana Decker'],
-//     isActive: true,
-//     balance: 2764,
-//     gender: 'female',
-//     age: 39
-//   }
-// ]
+const users: User[] = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39
+  }
+]
 
 // // Пиши код ниже этой строки
-// const getFriends = (users) => {
+// type GetFriends = (users: User[]) => string[];
+
+// const getFriends: GetFriends = (users) => {
 //   const allFriends = users.flatMap(user => user.friends);
-//  return allFriends.filter((friend, index, array) => array.indexOf(friend) === index);
+//   return allFriends.filter((friend, index, array) => array.indexOf(friend) === index);
 // };
-// // Пиши код выше этой строки
+// Пиши код выше этой строки
 
 // 31 - 6
 // Задание
 // Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, и получить общую сумму этих времён. Рассчитать время для каждого из игроков, можно разделив его время (свойство playtime) на количество игр (свойство gamesPlayed).
+// interface Player {
+//   readonly name: string;
+//   readonly playtime: number;
+//   readonly gamesPlayed: number;
+// };
 
-// const players = [
+// const players: Player[] = [
 //   { name: 'Манго', playtime: 1270, gamesPlayed: 4 },
 //   { name: 'Поли', playtime: 469, gamesPlayed: 2 },
 //   { name: 'Аякс', playtime: 690, gamesPlayed: 3 },
@@ -274,8 +312,8 @@
 // // Пиши код ниже этой строки
 
 // const totalAveragePlaytimePerGame = players.reduce((total, player) => {
-//     const onePlayTime = player.playtime / player.gamesPlayed;
-//     return onePlayTime + total;
+//   const onePlayTime = player.playtime / player.gamesPlayed;
+//   return onePlayTime + total;
 // }, 0);
 
 // console.log(totalAveragePlaytimePerGame);
@@ -285,8 +323,10 @@
 // Дополни функцию getTotalFriendCount(users) так, чтобы она считала и возвращала общее количество друзей(свойство friends) всех пользователей из массива users.
 
 // Пиши код ниже этой строки
-// const getTotalFriendCount = users => {
-//   return users.reduce((accum, user) => accum + user.friends.length, 0) 
+// type GetTotalFriendCount = (users: User[]) => number;
+
+// const getTotalFriendCount: GetTotalFriendCount = users => {
+//   return users.reduce((accum, user) => accum + user.friends.length, 0)
 // };
 // // Пиши код выше этой строки
 
@@ -296,11 +336,11 @@
 // Задание
 // Дополни функцию sortByDescendingFriendCount(users) так, чтобы она возвращала массив пользователей отсортированный по убыванию количества их друзей (свойство friends).
 
-// const sortByDescendingFriendCount = users => {
-//   return [...users].sort((a, b) => b.friends.length - a.friends.length) 
-// };
+const sortByDescendingFriendCount = users => {
+  return [...users].sort((a, b) => b.friends.length - a.friends.length)
+};
 
-// console.log(sortByDescendingFriendCount(users));
+console.log(sortByDescendingFriendCount(users));
 
 // 41 - 6
 // Задание
@@ -353,7 +393,9 @@
 //     return users
 //         .filter(user => user.gender === gender)
 //         .reduce((acc, total) => acc + total.balance, 0)
-    
+
 // };
 
 // console.log(getTotalBalanceByGender(users, 'female'));
+
+export { }
